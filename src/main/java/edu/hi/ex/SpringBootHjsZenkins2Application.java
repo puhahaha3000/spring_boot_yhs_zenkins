45faa3fed9e403076c5018be60deaa65f4e95f52
TEACHER\PC-00 <nqwrt@naver.com>
2022-07-19 16:55:36 +0900

    first commit

---
+++ src/main/java/edu/hi/ex/SpringBootHjsZenkins2Application.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsZenkins2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsZenkins2Application.class, args);
+	}
+
+}
---
+++ src/main/java/edu/hi/ex/controller/HomeController.java
@@ -0,0 +1,18 @@
+package edu.hi.ex.controller;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping
+	public String home() {
+		return "Hello Zenkins";
+	}
+
+
+}
